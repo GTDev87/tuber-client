a535d2eb11b9e7356fe53d949eff8b56eafb2651
Gregory Thompson <gregt87@gmail.com>
2015-10-16 01:49:27 -0400

    sent http request to server with received credentials

--- script/script.js
+++ script/script.js
@@ -6,13 +6,15 @@ var tuberClient = require("../lib"),
   secretFileLocation = args[2],
   macaroonFileLocation = args[3];
 
-tuberClient.createConnection(
-  ip, 
-  port, 
+var socketClearTextStream = tuberClient.createConnection(
   fs.readFileSync(secretFileLocation, "utf8"), 
-  JSON.parse(fs.readFileSync(macaroonFileLocation, "utf8")), 
-  function (socketClearTextStream){
-    
-
-
+  JSON.parse(fs.readFileSync(macaroonFileLocation, "utf8")),
+  "https://" + ip + ":" + port,
+  function (error, response, body) {
+    if(response.statusCode == 201){
+      console.log('document saved as: http://mikeal.iriscouch.com/testjs/'+ rand)
+    } else {
+      console.log('error: '+ response.statusCode)
+      console.log(body)
+    }
   });
