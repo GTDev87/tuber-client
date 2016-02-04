781e85338b881b914a8c303da4e2aa4f9360c4a8
Gregory Thompson <gregt87@gmail.com>
2016-02-04 00:59:59 -0500

    updated tuber client using node-macaroons

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -4,7 +4,16 @@ console.log("test started");
 var assert = require("assert"),
   tuberClient = require("../lib");
 
-describe('tuber-deploy', function () {
+describe('tuber-client', function () {
+  var err = null;
+
+  beforeEach(function(done) {
+    tuberClient.createConnection(secretPemLocation, macaroonWithCaveat, location, function (err, response, body) {
+      err = errObj;
+      done();
+    });
+  });
+
   it('should have unit test!', function () {
   });
 });
